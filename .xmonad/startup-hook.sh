0c019a93c894a097968004a903eadce855386d17
David Holm <david.holm@ericsson.com>
2018-07-09 09:09:30 +0200

    startup-hook: pgrep bugfix
    Use -f with pgrep so it will ignore the path to the process.

--- .xmonad/startup-hook.sh
+++ .xmonad/startup-hook.sh
@@ -1,28 +1,34 @@
 #!/usr/bin/env bash
 
-if hash keychain &>/dev/null; then
+if command -v keychain &>/dev/null; then
     keychain --quiet --quick "${HOME}/.ssh/id_rsa"
 fi
 
-if [ -z "$(pgrep -u ${USER} gnome-settings-daemon)" ]; then
-    gnome-settings-daemon &
+if command -v unity-settings-daemon 2>/dev/null; then
+    if [ -z "$(pgrep -u "${USER}" -f unity-settings-daemon)" ]; then
+        unity-settings-daemon &
+    fi
+elif command -v gnome-settings-daemon 2>/dev/null; then
+    if [ -z "$(pgrep -u "${USER}" gnome-settings-daemon)" ]; then
+         gnome-settings-daemon &
+    fi
 fi
 
-if [ -z "$(pgrep -u ${USER} xscreensaver)" ]; then
-    if [ -n "$(pgrep -u ${USER} gnome-screensaver)" ]; then
-        killall -9 gnome-screensaver
+if [ -z "$(pgrep -u "${USER}" -f xscreensaver)" ]; then
+    if [ -n "$(pgrep -u "${USER}" -f gnome-screensaver)" ]; then
+        kill -9 "$(pgrep -u "${USER}" -f gnome-screensaver)"
     fi
     xscreensaver -no-splash &
 fi
 
-if [ -z "$(pgrep -u ${USER} nm-applet)" ]; then
+if [ -z "$(pgrep -u "${USER}" nm-applet)" ]; then
     nm-applet &
 fi
 
-if [ -z "$(pgrep -u ${USER} taffybar)" ]; then
+if [ -z "$(pgrep -u "${USER}" taffybar)" ]; then
     taffybar &
 fi
 
 if [ -x "${HOME}/.local/bin/startup-hook.local.sh" ]; then
-    ${HOME}/.local/bin/startup-hook.local.sh
+    "${HOME}"/.local/bin/startup-hook.local.sh
 fi
